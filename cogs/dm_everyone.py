cbd89c35ee4909e8ce38532abe9350306168401a
Simo <128434490+simo665@users.noreply.github.com>
2024-10-07 22:24:16 +0100

    Add files via upload

---
+++ cogs/dm_everyone.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+import asyncio
+from config import TOKEN, intents
+
+class dm_everyone(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)  # Ensure only admins can use it
+    async def dm_all(self, ctx, guild_id: int, *, message: str):
+        
+        save_server=[1264302631174668299, 1282315062756769803] # User will be able to add their server using command later
+        
+        if guild_id in save_server:
+            await ctx.send("You can't use that command on that server.")
+            return 
+        
+        guild = self.bot.get_guild(guild_id)  # Get the guild
+        
+        if guild is None:  # Check if the guild exists
+            await ctx.send("Guild not found. Please provide a valid server ID.")
+            return 
+        print("Server exist!")
+        
+        guild_members = len([member for member in guild.members if not member.bot])  # Count non-bot members
+        
+        success_dm = 0  # Successful direct messages 
+        failed_dm = 0  # Failed direct messages 
+        loading_emoji = "🕑"  #"<a:loading_circle:1292501983516688518>"
+        dots_loading =  "⏳" #"<a:loading_dots:1292502000675586119>"
+
+        # Create an embed for the progress message
+        embed = discord.Embed(
+            title=f"DM Progress {dots_loading}",
+            description=f"Sending DMs to {guild_members} members in progress..\n✅ Successful dms: **{success_dm}**\n❌ Failed dms: **{failed_dm}**",
+            color=discord.Color.from_str("#f2f7f2")
+        )
+        
+        
+        progress_message = await ctx.send(embed=embed)
+        await progress_message.add_reaction(loading_emoji)
+        print("Embed sent!")
+        
+        
+        
+        skip_users = []
+        non_guild = 1264302631174668299
+        
+        
+        
+        for member in guild.members:
+            # add the members who has the non guild as a mutual server to the skip list
+            if self.bot.get_guild(non_guild) in member.mutual_guilds:
+                skip_users.append(member)
+                
+            if not member.bot:
+                try:
+                    # check if User in the skip list or not
+                    if not member in skip_users:
+                        await member.send(message)  # Send message
+                        success_dm += 1
+                    else:    
+                        print(f"{member.name} has been skipped.")
+                          
+                except discord.Forbidden:
+                    failed_dm += 1
+                    print(f"Failed to send DM to {member.name}: DMs are disabled.")  # Log the issue
+                except Exception as e:
+                    failed_dm += 1
+                    print(f"An error occurred while sending DM to {member.name}: {e}")  # Log the issue
+                # Update embed
+                embed.description=f"Sending DMs to {guild_members} members in progress..\n✅ Successful dms: **{success_dm}**\n❌ Failed dms: **{failed_dm}**"
+                await progress_message.edit(embed=embed)    
+                await asyncio.sleep(3) 
+           
+        await progress_message.remove_reaction(loading_emoji, self.bot.user)
+        embed.title="Finished. ✅"
+        embed.description=f"DMs sent to {success_dm} members. Failed to send to {failed_dm} members."
+        embed.color=discord.Color.from_str("#05e605")
+        await progress_message.edit(embed=embed)    
+
+async def setup(bot):
+    await bot.add_cog(dm_everyone(bot))
---
+++ cogs/dm_user.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands
+from config import TOKEN, intents
+import datetime
+
+class dm_user(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.active_conversations = {} # Dictionary to store user conversations
+        self.first_time_dm = {} # Dictionary to check if it's the first message from a user
+    # Command to send DM
+    @commands.command()
+    async def dm(self, ctx, user: discord.User, *, message):
+        try:
+            # Send DM to the user
+            time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+           
+            await user.send(f"**{ctx.author}:** {message}\n-# This message is not appropriate? [Click to report](https://discord.com/channels/1264302631174668299/1276072321127550987)\n-# {time}")
+            await ctx.send(f"Message sent successfully to {user.name}")
+            if user.id not in self.first_time_dm:
+                await user.send("You can reply to this message by just sending a message here.")
+                self.first_time_dm[user.id] = True
+           
+            
+            # Track the conversation
+            self.active_conversations[user.id] = ctx.author.id
+
+        except discord.Forbidden:
+            await ctx.send(f"Couldn't send a DM to {user.name}. They may have DMs disabled or blocked the bot.")
+        except discord.HTTPException:
+            await ctx.send("Failed to send the message due to an API error.")
+
+    # Listen for incoming direct messages
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Check if it's a direct message and not from a bot
+        if isinstance(message.channel, discord.DMChannel) and not message.author.bot:
+            recipient_id = self.active_conversations.get(message.author.id)
+
+            # If the user is replying to someone
+            if recipient_id:
+                recipient = self.bot.get_user(recipient_id)
+                if recipient:
+                    try:
+                        time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+                        # Forward the reply to the original sender
+                        await recipient.send(f"**{message.author}:** {message.content}\n-# do this cmd to reply `$dm {message.author} (your message here)`. {time}")
+                        #await asyncio.sleep(1)
+                        await message.channel.send(f"Your reply has been forwarded.")
+                        self.active_conversations[user.id] = message.author.id
+                    except discord.HTTPException:
+                        await message.channel.send("Failed to forward the message.")
+                else:
+                    await message.channel.send("Couldn't find the original user.")
+            else:
+                if not "$dm" in message.content:
+                    await message.channel.send("No active conversation to reply to.")
+                else:
+                    pass 
+                    
+        
+
+
+
+async def setup(bot):
+    await bot.add_cog(dm_user(bot))
