aab0cf2fb830143bde211188a416c6e790ec8b03
Simo <128434490+simo665@users.noreply.github.com>
2024-10-10 22:42:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,13 @@ import os
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-
-# variables 
-loding_heart = "<a:loading_heart:1294048090189332537>"
-red_hearts = "<a:redhearts:1294048122011521064>"
 # On ready event
 @bot.event
 async def on_ready():
-    print(f'Bot is online as {bot.user}')
+    print(f'{bot.user} is online')
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    if any(word in message.content.lower() for word in ("welcome", "wlc")):
-        await message.add_reaction(loading_heart)
-        await asyncio.sleep(1.8)
-        await message.remove_reaction(loading_heart, bot.user)
-        await asyncio.sleep(0.1)
-        await message.add_reaction(red_hearts)
     await bot.process_commands(message)
 
 
