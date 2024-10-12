bd5c9c85f0549ce9b1f300274ddab796fe3f3ff6
Simo <128434490+simo665@users.noreply.github.com>
2024-10-12 16:52:18 +0100

    Update dm_user.py

--- cogs/dm_user.py
+++ cogs/dm_user.py
@@ -1,25 +1,28 @@
 import discord
 from discord.ext import commands
-from config import TOKEN, intents
 import datetime
 
 class dm_user(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.active_conversations = {} # Dictionary to store user conversations
-        self.first_time_dm = {} # Dictionary to check if it's the first message from a user
-    # Command to send DM
+        self.active_conversations = {}  # Dictionary to store user conversations
+        self.first_time_dm = {}  # Dictionary to check if it's the first message from a user
+
+    # Command to send a DM
     @commands.command()
     async def dm(self, ctx, user: discord.User, *, message):
         try:
-            # Send DM to the user
-            await user.send(f"**{ctx.author}:** {message}\n-# This message is not appropriate? [Click to report](https://discord.com/channels/1264302631174668299/1276072321127550987)")
+            # Get the current time
+            time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+            # Send the DM to the user
+            await user.send(f"**{ctx.author}:** {message}\n-# This message is not appropriate? [Click to report](https://discord.com/channels/1264302631174668299/1276072321127550987)\n-# {time}")
             await ctx.send(f"Message sent successfully to {user.name}")
+
+            # Check if it's the user's first message
             if user.id not in self.first_time_dm:
                 await user.send("You can reply to this message by just sending a message here.")
                 self.first_time_dm[user.id] = True
-           
-            
+
             # Track the conversation
             self.active_conversations[user.id] = ctx.author.id
 
@@ -40,20 +43,17 @@ class dm_user(commands.Cog):
                 recipient = self.bot.get_user(recipient_id)
                 if recipient:
                     try:
-                     # Forward the reply to the original sender
-                        await recipient.send(f"**{message.author}:** {message.content}\n-# do this cmd to reply `$dm {message.author} (your message here)`.")
-                        #await asyncio.sleep(1)
-                        await message.channel.send(f"Your reply has been forwarded.")
-                        self.active_conversations[user.id] = message.author.id
+                        time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+                        # Forward the reply to the original sender
+                        await recipient.send(f"**{message.author}:** {message.content}\n-# To reply, use the `$dm {message.author} (your message here)` command.\n-# {time}")
+                        await message.channel.send("Your reply has been forwarded.")
                     except discord.HTTPException:
                         await message.channel.send("Failed to forward the message.")
                 else:
                     await message.channel.send("Couldn't find the original user.")
             else:
-                if not "$dm" in message.content:
+                if "$dm" not in message.content:
                     await message.channel.send("No active conversation to reply to.")
-                else:
-                    pass 
-                    
+
 async def setup(bot):
     await bot.add_cog(dm_user(bot))
