bdd8d43171f0fdcdfeb8c67e881df69f345ddfaf
Simo <128434490+simo665@users.noreply.github.com>
2024-10-19 19:45:49 +0100

    Update TetrisGame.py

--- cogs/TetrisGame.py
+++ cogs/TetrisGame.py
@@ -1,9 +1,9 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import random
 import asyncio
 
-class TetrisGame(commands.Cog):
+class Tetris(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.board = []
@@ -11,97 +11,155 @@ class TetrisGame(commands.Cog):
         self.num_of_cols = 10
         self.empty_square = ':black_large_square:'
         self.colors = {
-            "blue": ':blue_square:',
-            "brown": ':brown_square:',
-            "orange": ':orange_square:',
-            "yellow": ':yellow_square:',
-            "green": ':green_square:',
-            "purple": ':purple_square:',
-            "red": ':red_square:'
+            "I": ':blue_square:',
+            "O": ':yellow_square:',
+            "T": ':purple_square:',
+            "J": ':brown_square:',
+            "L": ':orange_square:',
+            "S": ':green_square:',
+            "Z": ':red_square:'
         }
-        self.embed_colour = 0x077ff7  # Colour of line on embeds
+        self.current_piece = None
+        self.current_position = (0, 4)
+        self.current_rotation = 0
         self.points = 0
-        self.lines = 0
-        self.down_pressed = False
-        self.rotate_clockwise = False
-        self.rotation_pos = 0
-        self.h_movement = 0
-        self.is_new_shape = False
-        self.start_higher = False
+        self.channel = None
+        self.message = None
         self.game_over = False
-        self.index = 0
-        self.current_message = None
+        self.tetris_task = None
 
+        self.shapes = self.get_shapes()
         self.make_empty_board()
 
     def make_empty_board(self):
         self.board = [[self.empty_square for _ in range(self.num_of_cols)] for _ in range(self.num_of_rows)]
 
-    def format_board_as_str(self):
-        board_as_str = ''
+    def get_shapes(self):
+        return {
+            "I": [
+                [[1, 0], [1, 1], [1, 2], [1, 3]],
+                [[0, 2], [1, 2], [2, 2], [3, 2]],
+            ],
+            "O": [
+                [[0, 1], [0, 2], [1, 1], [1, 2]]
+            ],
+            "T": [
+                [[0, 1], [1, 0], [1, 1], [1, 2]],
+                [[0, 1], [1, 1], [1, 2], [2, 1]],
+                [[1, 0], [1, 1], [1, 2], [2, 1]],
+                [[0, 1], [1, 0], [1, 1], [2, 1]],
+            ],
+            # Add more shapes (J, L, S, Z) here
+        }
+
+    def format_board(self):
+        board_str = ""
         for row in self.board:
-            board_as_str += ''.join(row) + "\n"
-        return board_as_str
+            board_str += "".join(row) + "\n"
+        return board_str
 
-    def get_random_shape(self):
-        shapes = [
-            [[0, 3], [0, 4], [0, 5], [0, 6]],  # Example shape
-            # Add more shapes based on the original code
-        ]
-        random_shape = random.choice(shapes)
-        return random_shape
+    def can_move(self, shape, position):
+        for block in shape:
+            row = block[0] + position[0]
+            col = block[1] + position[1]
+            if row < 0 or row >= self.num_of_rows or col < 0 or col >= self.num_of_cols:
+                return False
+            if self.board[row][col] != self.empty_square:
+                return False
+        return True
 
-    async def reset_game(self):
-        self.make_empty_board()
-        self.down_pressed = False
-        self.rotate_clockwise = False
-        self.rotation_pos = 0
-        self.h_movement = 0
-        self.is_new_shape = False
-        self.start_higher = False
+    def add_piece_to_board(self, shape, position, piece_color):
+        for block in shape:
+            row = block[0] + position[0]
+            col = block[1] + position[1]
+            self.board[row][col] = self.colors[piece_color]
+
+    def remove_piece_from_board(self, shape, position):
+        for block in shape:
+            row = block[0] + position[0]
+            col = block[1] + position[1]
+            self.board[row][col] = self.empty_square
+
+    def clear_lines(self):
+        full_rows = [i for i, row in enumerate(self.board) if all(square != self.empty_square for square in row)]
+        for row in full_rows:
+            del self.board[row]
+            self.board.insert(0, [self.empty_square for _ in range(self.num_of_cols)])
+        self.points += len(full_rows)
+
+    async def run_game(self):
         self.game_over = False
-        self.points = 0
-        self.lines = 0
+        self.current_piece = random.choice(list(self.shapes.keys()))
+        self.current_rotation = 0
+        self.current_position = (0, 4)
 
-    async def run_game(self, ctx, msg, cur_shape):
-        self.is_new_shape = True
         while not self.game_over:
-            await asyncio.sleep(1)  # Update the interval as needed
-            # Update game logic here
-            embed = discord.Embed(description=self.format_board_as_str(), color=self.embed_colour)
-            await msg.edit(embed=embed)
-            self.is_new_shape = False
+            shape = self.shapes[self.current_piece][self.current_rotation]
+            if self.can_move(shape, self.current_position):
+                self.add_piece_to_board(shape, self.current_position, self.current_piece)
+                embed = discord.Embed(title="Tetris", description=self.format_board(), color=0x077ff7)
+                await self.message.edit(embed=embed)
+                await asyncio.sleep(1)
+                self.remove_piece_from_board(shape, self.current_position)
+                self.current_position = (self.current_position[0] + 1, self.current_position[1])
+            else:
+                self.add_piece_to_board(shape, (self.current_position[0] - 1, self.current_position[1]), self.current_piece)
+                self.clear_lines()
+                self.current_piece = random.choice(list(self.shapes.keys()))
+                self.current_rotation = 0
+                self.current_position = (0, 4)
+                if not self.can_move(self.shapes[self.current_piece][self.current_rotation], self.current_position):
+                    self.game_over = True
+                    await self.channel.send("Game Over!")
+                    break
 
-    @commands.command()
+    @commands.command(name="tstart")
     async def start_tetris(self, ctx):
-        """Start a Tetris game."""
-        await self.reset_game()
-        embed = discord.Embed(
-            title="Tetris in Discord",
-            description=self.format_board_as_str(),
-            color=self.embed_colour
-        )
-        embed.add_field(
-            name="How to Play:",
-            value="Use ⬅ ⬇ ➡ to move left, down, and right respectively. \n🔃 to rotate.",
-            inline=False
-        )
-        self.current_message = await ctx.send(embed=embed)
-        await self.current_message.add_reaction("▶")
+        """Starts a game of Tetris."""
+        self.channel = ctx.channel
+        self.make_empty_board()
+        embed = discord.Embed(title="Tetris", description=self.format_board(), color=0x077ff7)
+        self.message = await ctx.send(embed=embed)
+        if self.tetris_task:
+            self.tetris_task.cancel()
+        self.tetris_task = asyncio.create_task(self.run_game())
 
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if user == self.bot.user:
+    @commands.command(name="rotate")
+    async def rotate(self, ctx):
+        """Rotates the current piece."""
+        if self.game_over:
+            await ctx.send("The game is over! Start a new one with `!start_tetris`.")
             return
-        if str(reaction.emoji) == "▶" and reaction.message == self.current_message:
-            await self.reset_game()
-            await reaction.message.clear_reactions()
-            await reaction.message.add_reaction("⬅")
-            await reaction.message.add_reaction("⬇")
-            await reaction.message.add_reaction("➡")
-            await reaction.message.add_reaction("🔃")
-            starting_shape = self.get_random_shape()
-            await self.run_game(None, reaction.message, starting_shape)
+        shape = self.shapes[self.current_piece][self.current_rotation]
+        self.remove_piece_from_board(shape, self.current_position)
+        self.current_rotation = (self.current_rotation + 1) % len(self.shapes[self.current_piece])
+        shape = self.shapes[self.current_piece][self.current_rotation]
+        if not self.can_move(shape, self.current_position):
+            self.current_rotation = (self.current_rotation - 1) % len(self.shapes[self.current_piece])
+        self.add_piece_to_board(shape, self.current_position, self.current_piece)
+
+    @commands.command(name="move")
+    async def move(self, ctx, direction: str):
+        """Move the piece left, right, or down."""
+        if self.game_over:
+            await ctx.send("The game is over! Start a new one with `!start_tetris`.")
+            return
+        shape = self.shapes[self.current_piece][self.current_rotation]
+        self.remove_piece_from_board(shape, self.current_position)
+
+        if direction.lower() == "left":
+            new_position = (self.current_position[0], self.current_position[1] - 1)
+        elif direction.lower() == "right":
+            new_position = (self.current_position[0], self.current_position[1] + 1)
+        elif direction.lower() == "down":
+            new_position = (self.current_position[0] + 1, self.current_position[1])
+        else:
+            await ctx.send("Invalid direction! Use 'left', 'right', or 'down'.")
+            return
+
+        if self.can_move(shape, new_position):
+            self.current_position = new_position
+        self.add_piece_to_board(shape, self.current_position, self.current_piece)
 
 async def setup(bot):
-    await bot.add_cog(TetrisGame(bot))
+    await bot.add_cog(Tetris(bot))
