e6d87cc7216b5184df4257d1a71b631b96bdebbe
Simo <128434490+simo665@users.noreply.github.com>
2024-10-19 21:19:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import discord
+from discord.ext import commands
+import asyncio
+from config import TOKEN, intents
+import webserver 
+import os
 
+bot = commands.Bot(command_prefix='m!', intents=intents)
+bot.remove_command("help")
+
+# On ready event
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is online')
+
+
+async def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
