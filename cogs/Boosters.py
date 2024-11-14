0dca954e6134cf551ab893005323e33fa003beeb
Simo <128434490+simo665@users.noreply.github.com>
2024-11-14 13:48:01 +0100

    Update Boosters.py

--- cogs/Boosters.py
+++ cogs/Boosters.py
@@ -8,12 +8,44 @@ class Boosters(commands.Cog):
         self.channel_id = None
 
     async def create_boosters_embed(self, guild):
-        """Create and return the embed with the list of boosters."""
+        """Create and return the embed with the list of boosters and the perks."""
         boosters_role = guild.get_role(self.boosters_role_id)
         if boosters_role:
             # Get members who have the boosters role
-            boosters_names = [member.name for member in guild.members if boosters_role in member.roles]
-            embed = discord.Embed(title="Boosters", description="\n".join(boosters_names) if boosters_names else "No boosters yet.")
+            boosters_names = [f"<@{member.id}>" for member in guild.members if boosters_role in member.roles]
+            
+            # Define the perks section
+            perks_section = """
+            ## Boosts perks:
+            _ _ 
+            <:02_animegirlpeek:1264992017826713725> **1 Boost:**
+            - <:emoji_274:1272175998733123585> **Leveling Boost x30**
+            - <:emoji_274:1272175998733123585> **Skip 5 Levels**
+            - <:emoji_274:1272175998733123585> **Media Permission**
+            - <:emoji_274:1272175998733123585> **Exclusive Booster Role**
+
+            <:02_animegirloop:1264961707680202802> **2 Boosts:**
+            - <:emoji_274:1272175998733123585> **All previous perks +**:
+            - <:emoji_274:1272175998733123585> **Skip 10 Levels**
+            - <:emoji_274:1272175998733123585> **Two Custom Emojis**
+            - <:emoji_274:1272175998733123585> **VIP Role**
+
+            <:02_animegirlyay:1264992016605909076> **3 or More Boosts:**
+            - <:emoji_274:1272175998733123585> **All previous perks +**:
+            - <:emoji_274:1272175998733123585> **1 Custom Role**
+            - <:emoji_274:1272175998733123585> **1 Custom VC Channel**
+
+            > Your support helps our community grow! Boosting gives you awesome perks and helps create a better server for everyone!
+
+            -# **Note**: You get "Media Permission" automatically, but for other perks, we will grant them manually. Please create a **[ticket](https://discord.com/channels/1264302631174668299/1264350097118859294)** to claim your perks.
+            """
+            
+            # Append the list of boosters names
+            boosters_list = "\n".join(boosters_names) if boosters_names else "No boosters yet."
+            
+            embed = discord.Embed(title="Boosters", description=perks_section)
+            embed.add_field(name="Boosters", value=boosters_list, inline=False)
+            
             return embed
         return None
 
