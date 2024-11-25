a22b6a05b70f58dd30bb84cf95d31a2bbe57c25d
Simo <128434490+simo665@users.noreply.github.com>
2024-11-25 11:29:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,16 @@ import discord
 
 # Bot requirements 
 load_dotenv()
-TOKEN=os.getenv('BOT_TOKEN')
-api_key=os.environ["MISTRAL_API_KEY"]
-
-intents = discord.Intents.default()
+TOKEN=os.getenv('BOT_TOKEN') # Bot token 
+api_key=os.environ["MISTRAL_API_KEY"] # Ai API
+# Bot Intents
+intents = discord.Intents.default() 
 intents.presences = True 
 intents.members = True 
 intents.message_content = True  
 intents.voice_states = True
+
+# Ai bot configs
+personality = "You're a little naughty girl, you write informally like a human, your replies always short" # bot personality details 
+knowledge = "" # Include here what you want the bot to know about.. 
+memory_length = 10
